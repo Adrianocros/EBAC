d18bd4d33b5e0497b9afe92b1b5422390b53d7da
Adrianocros <49080698+Adrianocros@users.noreply.github.com>
2025-04-06 19:25:43 -0300

    Calculos JS

---
+++ CalculoHosra/script.js
@@ -0,0 +1,13 @@
+function cinema(){
+    const titulo = document.getElementById("titulo").value;
+    const duracao = document.getElementById("duracao").value;
+
+
+    const horas = Math.floor(duracao / 60);
+    const minutos = duracao - horas * 60;
+
+    document.getElementById("tituloSaida").textContent = titulo.toUpperCase();
+    document.getElementById("duracaoSaida").textContent = horas + "Hotas(s) e " + minutos + "Minutos(s)"
+
+}
+
